7a7d69ed315748173936d051f616cb2f232e0cf6
prathame <prathamgi.pg@gmail.com>
2023-08-29 12:36:14 +0530

    day2 completed

--- mysql/day1.sql
+++ mysql/day1.sql
@@ -9,5 +9,7 @@ CREATE TABLE `Employees` (
 );
 INSERT INTO Employees VALUES(1, 'Mark', 'mark@gmail.com', 'IT',' 2000-05-28', 50000.00, true);
 INSERT INTO Employees VALUES(2, 'John', 'john@gmail.com', 'HR',' 1995-05-28', 10000.00, true);
+INSERT INTO Employees VALUES(3, 'Jay', 'jay@gmail.com', 'Intern',' 2005-06-08', 4000.00, true);
+INSERT INTO Employees VALUES(4, 'James', 'james@gmail.com', 'Legal',' 2000-09-18', 34000.00, true);
+INSERT INTO Employees VALUES(5, 'Ethan', 'ethan@gmail.com', 'IT',' 1999-04-02', 24000.00, true);
 
-select * from `Employees`;
---
+++ mysql/day2.sql
@@ -0,0 +1,3 @@
+select * from `Employees`;
+
+SELECT ID , NAME FROM Employees WHERE `Salary`>10000;
